308cc223c78f88afaad31dbcebc6021d28ede953
Wenqi-Lu <wenqi-lu@outlook.com>
2025-07-07 14:22:19 -0400

    update cv and working paper

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -9,14 +9,16 @@
     <a class="social-media" href="">
       <fa :icon="['fab', 'instagram']" class="fa-lg"/>
     </a> -->
-    <a class="social-media" href="mailto:wenqi.lu@uantwerpen.be">
-      <fa :icon="['fas', 'envelope']" class="fa-lg" style="margin-right: 0.5em;" />
-      <span>wenqi.lu@uantwerpen.be</span>
-    </a>
     <a class="social-media" href="mailto:wenqi.lu@ulb.be">
       <fa :icon="['fas', 'envelope']" class="fa-lg" style="margin-right: 0.5em;" />
       <span>wenqi.lu@ulb.be</span>
     </a>
+
+    <a class="social-media" href="mailto:wenqi.lu@uantwerpen.be">
+      <fa :icon="['fas', 'envelope']" class="fa-lg" style="margin-right: 0.5em;" />
+      <span>wenqi.lu@uantwerpen.be</span>
+    </a>
+
   </div>
 </template>
 
