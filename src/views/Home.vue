e14be3ce1a4b2855f6eec7e84b3600b6609fd969
Wenqi-Lu <wenqi-lu@outlook.com>
2026-06-26 05:25:50 -0400

    docs(Home): update intro format

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <p>
-      I’m a PhD candidate in Economics at the University of Antwerp and Université Libre de Bruxelles, ECARES.
+      I’m a PhD candidate in Economics at ECARES, Université Libre de Bruxelles and the University of Antwerp.
       My research interests lie in <strong>family economics, labor economics and revealed preference theory</strong>.
     </p>
     <p>
